031195086c1b95e05efa1ad9a5fbe510d8671f76
ivn <iwanaev23@gmail.com>
2021-03-01 23:57:23 +0300

    Added function backup

---
+++ logic/Backup.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace MedIS.logic
+{
+    class Backup
+    {
+
+        public void import(OpenFileDialog fileDialog)
+        {
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "cmd.exe";
+
+            startInfo.Arguments = "/c \"\"C:\\Program Files\\MySQL\\MySQL Server 8.0\\bin\\MySQL.exe\" -uroot -h127.0.0.1 -P3306 -pVfhnvfhn23@ mis < \"" + fileDialog.FileName + "\"\"";
+            process.StartInfo = startInfo;
+            process.Start();
+
+        }
+
+        public void export(SaveFileDialog saveFile)
+        {
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "cmd.exe";
+            saveFile.Filter = "MySQL Text File|*.sql";
+            saveFile.FileName = "Backup LIS";
+            if (saveFile.ShowDialog() == DialogResult.OK) {
+                startInfo.Arguments = "/c \"\"C:\\Program Files\\MySQL\\MySQL Server 8.0\\bin\\MySQLdump.exe\" -uroot -h127.0.0.1 -P3306 -pVfhnvfhn23@ mis > \"" + saveFile.FileName + "\"\"";
+                process.StartInfo = startInfo;
+                process.Start();
+            }
+
+        }
+    }
+}
