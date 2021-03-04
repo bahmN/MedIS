8b219f8570c14e93d52d358d225cb97ac250187a
ivn <iwanaev23@gmail.com>
2021-03-04 14:37:25 +0300

    New form for appoinments

---
+++ logic/DoctorAppointment.cs
@@ -0,0 +1,27 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace MedIS.logic
+{
+    class DoctorAppointment
+    {
+        public MySqlCommand addAppoinment(string numbPatient, object numbDoctor, string Date, string Ambul, string Dispanser, string Note)
+        {
+            Connection con = new Connection();
+            MySqlCommand cAdd = new MySqlCommand("INSERT INTO `записи к врачу`(`Номер пациента`, `Номер врача`, `Дата обращения`, `Амбулаторное лечение`, `Диспансер`, Примечание)" +
+                " VALUES ('" + numbPatient + "', '" + numbDoctor + "', '" + Date + "', '" + Ambul + "', '" + Dispanser + "', '" + Note + "')", con.connect());
+            cAdd.ExecuteNonQuery();
+
+            return cAdd;
+        }
+
+        public MySqlCommand changeAppoinment(string PK, string FN, string Pos, string Spec, string Note)
+        {
+            Connection con = new Connection();
+            MySqlCommand cChng = new MySqlCommand("UPDATE персонал SET `ФИО Врача`= '" + FN + "', Должность= '" + Pos + "', Примечание= '" + Note + "'" +
+                ", Специализация= '" + Spec + "' WHERE `Номер врача`= '" + PK + "'", con.connect());
+            cChng.ExecuteNonQuery();
+
+            return cChng;
+        }
+    }
+}
---
+++ screens/frmAddAppoinment.cs
@@ -0,0 +1,50 @@
+﻿using MedIS.logic;
+using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace MedIS.screens
+{
+    public partial class frmAddAppoinment : Form
+    {
+        public frmAddAppoinment()
+        {
+            InitializeComponent();
+        }
+        public string numbPatient;
+        private void bttnAccept_Click(object sender, EventArgs e)
+        {
+            var numbDoctor = cbDoctor;
+            string date = dateTimePicker.Text;
+            string ambul = cbAmbul.Text;
+            string dispanser = cbDispancer.Text;
+            string note = tbNote.Text;
+            DoctorAppointment add = new DoctorAppointment();
+            add.addAppoinment(numbPatient, numbDoctor.SelectedValue, date, ambul, dispanser, note);
+            Close();
+        }
+
+        private void frmAddAppoinment_Load(object sender, EventArgs e)
+        {
+            Connection con = new Connection();
+            // Заполнение comboBox'a врачами
+            MySqlDataAdapter commandABP = new MySqlDataAdapter("SELECT `Номер врача`, `ФИО врача` FROM персонал", con.connect());
+            DataTable dtABP = new DataTable();
+            commandABP.Fill(dtABP);
+            cbDoctor.DataSource = dtABP;
+            cbDoctor.DisplayMember = "ФИО врача";
+            cbDoctor.ValueMember = "Номер врача";
+            cbDoctor.SelectedIndex = -1;
+
+            //Параметры dateTimePicker
+            dateTimePicker.MinDate = DateTime.Now;
+            dateTimePicker.MaxDate = DateTime.Now;
+        }
+
+        private void bttnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
